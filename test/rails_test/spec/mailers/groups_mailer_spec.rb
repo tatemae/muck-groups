0cb029dabd9d9989795d8d71aeb4ed7b99cc5a92
Justin Ball <justinball@gmail.com>
2010-10-15 00:31:23 -0600

    added spec for groups mailer

--- test/rails_test/spec/mailers/groups_mailer_spec.rb
+++ test/rails_test/spec/mailers/groups_mailer_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe FriendMailer do
+describe GroupsMailer do
 
   def setup
     ActionMailer::Base.delivery_method = :test
@@ -8,22 +8,12 @@ describe FriendMailer do
     ActionMailer::Base.deliveries = []
   end
   
-  it "should send follow email" do
+  it "should send invite email" do
     inviter = Factory(:user)
     invited = Factory(:user)
-    email = FriendMailer.follow(inviter, invited).deliver
+    email = GroupsMailer.invite(inviter, group, email, name, subject, message).deliver
     ActionMailer::Base.deliveries.should_not be_empty
-    email.to.should == [invited.email]
-    email.from.should == [MuckEngine.configuration.from_email]
-  end
-  
-  it "should send friend_request email" do
-    inviter = Factory(:user)
-    invited = Factory(:user)
-    response = FriendMailer.deliver_friend_request(inviter, invited)
-    ActionMailer::Base.deliveries.should_not be_empty
-    email = ActionMailer::Base.deliveries.last
-    email.to.should == [invited.email]
+    email.to.should == [email]
     email.from.should == [MuckEngine.configuration.from_email]
   end
   
